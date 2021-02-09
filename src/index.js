dccc721127294c03e640ec969fac2d81ed344ade
NikitaKulik <kuliknikita@gmail.com>
2021-02-09 22:45:35 +0300

    feat: implement brackets checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsArray = str.split("")
+  let bracketsPairs = {}
+  let openBrackets = []
+  let isCorrect = true
+
+  bracketsConfig.forEach(pair => {
+    bracketsPairs[pair[1]] = pair[0];
+  });
+
+  bracketsArray.forEach(bracket => {
+    if (!bracketsPairs[bracket]) {
+      openBrackets.push(bracket);
+    } else if (bracketsPairs[bracket] == bracket) {
+      if (bracket == openBrackets[openBrackets.length - 1]) {
+        openBrackets.pop()
+      } else {
+        openBrackets.push(bracket);
+      }
+    } else if (bracketsPairs[bracket] === openBrackets[openBrackets.length - 1]) {
+      openBrackets.pop();
+      } else {
+        isCorrect = false
+      }
+    })
+
+  if (openBrackets.length !== 0) {
+    isCorrect = false
+  }
+
+  return isCorrect
 }
